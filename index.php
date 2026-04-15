a2a730800e36b00ec0bf6e4e440ff520a975963a
jacobhasason <154370419+jacobhasason@users.noreply.github.com>
2026-04-15 16:17:36 -0700

    4/15 - Example Iteration

--- index.php
+++ index.php
@@ -4,22 +4,24 @@
 <main>
     <?php include 'view/aside.php'; ?>
 
-    <body
-       <div class="source-container">
-  <div class="source-item">
-    <div class="thumbnail"></div>
-    <div class="source-details">
-      <h3><strong>Title:</strong> Does AI Improve Agriculture Production?</h3>
-      <p><strong>Author:</strong> John Smith</p>
-      <p><strong>Date:</strong> 4/15/2026</p>
-    </div>
-    <div class="source-icon">
-      <span class="icon">🔊</span>
-    </div>
-  </div>
-
-
-</div>
+    <body>
+        <ul>
+            <div class="source-container">
+                <li>
+                    <div class="source-item">
+                        <div class="thumbnail"></div>
+                        <div class="source-details">
+                            <h3><strong>Title:</strong> Does AI Improve Agriculture Production?</h3>
+                            <p><strong>Author:</strong> John Smith</p>
+                            <p><strong>Date:</strong> 4/15/2026</p>
+                        </div>
+                        <div class="source-icon">
+                            <span class="icon">🔊</span>
+                        </div>
+                    </div>
+                </li>
+            </div>
+        </ul>
     </body>
 </main>
 <script src="scripts/date.js"></script>
