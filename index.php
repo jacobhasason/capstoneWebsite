19467d7443136f5891d5f45d1becd691a98cf81b
jacobhasason <154370419+jacobhasason@users.noreply.github.com>
2026-04-16 16:10:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                         <div class="thumbnail"></div>
                         <div class="source-details">
                             <h3><strong>Title:</strong> Does AI Improve Agriculture Production?</h3>
-                            <p><strong>Author:</strong> John Smith</p>
+                            <p><strong>Author:</strong> Mike Smith</p>
                             <p><strong>Date:</strong> 4/15/2026</p>
                         </div>
                         <div class="source-icon">
