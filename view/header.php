35b38ba79cee60cf8159f1ff513a52deb11c8957
jacobhasason <154370419+jacobhasason@users.noreply.github.com>
2026-04-16 09:01:53 -0700

    Update header.php

--- view/header.php
+++ view/header.php
@@ -13,12 +13,7 @@
             <link rel="stylesheet" href="styles/jquery.bxslider.css">
             <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
             <link rel="stylesheet" href="styles/main.css">
-            <link rel="stylesheet" href="styles/support.css">
-            <link rel="stylesheet" href="styles/shipping.css">
-            <link rel="stylesheet" href="styles/products.css">
-            <link rel="stylesheet" href="styles/guitars.css">
-            <link rel="stylesheet" href="styles/customer_login.css">
-            <link rel="stylesheet" href="styles/customer.css">
+           
 
         </header>
     </body>
