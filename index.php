d29cdb3937a1130e52f4bc17823ebb35a440ab82
jacobhasason <154370419+jacobhasason@users.noreply.github.com>
2026-04-27 15:33:38 -0700

    4/27 Source Names Update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <a href="ListingInfo.php?id=1" class="item-link">
                 <div class="thumbnail"></div>
                 <div class="source-details">
-                    <h3><strong>Title:</strong> Does AI Improve Agriculture Production?</h3>
+                    <h3><strong>Title:</strong> Source 1</h3>
                     <p><strong>Author:</strong> Mike Smith</p>
                     <p><strong>Date:</strong> 4/8/2026</p>
                 </div>
@@ -23,7 +23,7 @@
             <a href="ListingInfo.php?id=1" class="item-link">
                 <div class="thumbnail"></div>
                 <div class="source-details">
-                    <h3><strong>Title:</strong> Does AI Improve Fuckass projects?</h3>
+                    <h3><strong>Title:</strong> Source 2</h3>
                     <p><strong>Author:</strong> Saul Rodriguez</p>
                     <p><strong>Date:</strong> 4/8/2026</p>
                 </div>
@@ -38,7 +38,7 @@
             <a href="ListingInfo.php?id=1" class="item-link">
                 <div class="thumbnail"></div>
                 <div class="source-details">
-                    <h3><strong>Title:</strong> The Journey to Regain Wrist Movement</h3>
+                    <h3><strong>Title:</strong> Source 3</h3>
                     <p><strong>Author:</strong> Saul Rodriguez</p>
                     <p><strong>Date:</strong> 4/8/2026</p>
                 </div>
@@ -52,7 +52,7 @@
             <a href="ListingInfo.php?id=1" class="item-link">
                 <div class="thumbnail"></div>
                 <div class="source-details">
-                    <h3><strong>Title:</strong> I Survived Mista White</h3>
+                    <h3><strong>Title:</strong> Source 4 </h3>
                     <p><strong>Author:</strong> Saul Rodriguez</p>
                     <p><strong>Date:</strong> 4/8/2026</p>
                 </div>
@@ -62,33 +62,6 @@
             </a>
         </div>   
         
-         <div class="source-item">
-            <a href="ListingInfo.php?id=1" class="item-link">
-                <div class="thumbnail"></div>
-                <div class="source-details">
-                    <h3><strong>Title:</strong> Exploring the Zhu Curve</h3>
-                    <p><strong>Author:</strong> Saul Rodriguez</p>
-                    <p><strong>Date:</strong> 4/8/2026</p>
-                </div>
-                <div class="source-icon">
-                    <span class="icon">🔊</span>
-                </div>
-            </a>
-        </div>   
-        
-         <div class="source-item">
-            <a href="ListingInfo.php?id=1" class="item-link">
-                <div class="thumbnail"></div>
-                <div class="source-details">
-                    <h3><strong>Title:</strong> The Psychology Behind the Spinning Fish</h3>
-                    <p><strong>Author:</strong> Saul Rodriguez</p>
-                    <p><strong>Date:</strong> 4/8/2026</p>
-                </div>
-                <div class="source-icon">
-                    <span class="icon">🔊</span>
-                </div>
-            </a>
-        </div>   
            
     </body>
     <a href="AddSource.php" class="fab">+</a>
