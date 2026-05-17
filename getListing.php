1267f3711b32293c8e2fe00743d7bfe2021ee01d
HoldRob123 <holden.robinson@comcast.net>
2026-05-17 15:14:46 -0700

    Update getListing.php

--- getListing.php
+++ getListing.php
@@ -76,9 +76,9 @@ foreach ($listings as $listing): ?>
             </div>
 
             <div class="source-details">
-                <h3><?= htmlspecialchars($listing['title']) ?></h3>
-                <p><?= htmlspecialchars($listing['author']) ?></p>
-                <p><?= htmlspecialchars($listing['date']) ?></p>
+                <h3><?= htmlspecialchars($listing['title'] ?? '') ?></h3>
+                <p><?= htmlspecialchars($listing['author'] ?? '') ?></p>
+                <p><?= htmlspecialchars($listing['date'] ?? '') ?></p>
             </div>
 
             <div class="source-icon">
