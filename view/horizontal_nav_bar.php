e74094f82f52a7ed06c9ad033f857e8cf7e699b2
HoldRob123 <holden.robinson@comcast.net>
2026-05-19 16:01:33 -0700

    Update horizontal_nav_bar.php

--- view/horizontal_nav_bar.php
+++ view/horizontal_nav_bar.php
@@ -14,102 +14,110 @@
                 <!-- TOPIC -->
                 <li class="topic-menu">
                     <a href="#">Topic</a>
-    
-                    <ul class="dropdown topic-dropdown">
-    
-                        <!-- MACHINE LEARNING -->
+                
+                    <ul class="dropdown">
+                
+                        <!-- LEVEL 1: ML VKD -->
                         <li>
-                            <a href="#" class="expand-btn">
+                            <a href="#">
+                                <span class="checkbox-box"></span>
                                 Machine Learning with VKD
                             </a>
-    
-                            <ul class="subtopics hidden">
-    
-                                <!-- CLASSIFICATION -->
+                
+                            <ul class="dropdown">
+                
+                                <!-- LEVEL 2: CLASSIFICATION -->
                                 <li>
-                                    <a href="#" class="expand-btn sub-btn">
+                                    <a href="#">
+                                        <span class="checkbox-box"></span>
                                         Classification and Clustering
                                     </a>
-    
-                                    <ul class="subtopics hidden">
-    
-                                        <li>
-                                            <a href="#">
-                                                <span class="checkbox-box"></span>
-                                                Decision Trees & Generalized Decision Trees
-                                            </a>
-                                        </li>
-    
-                                        <li>
-                                            <a href="#">
-                                                <span class="checkbox-box"></span>
-                                                Support Vector Machine
-                                            </a>
-                                        </li>
-    
-                                        <li>
-                                            <a href="#">
-                                                <span class="checkbox-box"></span>
-                                                k-Nearest Neighbors
-                                            </a>
-                                        </li>
-    
+                
+                                    <ul class="dropdown">
+                
+                                        <li><a href="#"><span class="checkbox-box"></span>Decision Trees & Generalized Decision Trees</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Additive Classifiers (Linear, Quadratic, Generalized)</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Support Vector Machine</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>k-Nearest Neighbors</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Convolutional Neural Network</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Hyper-rectangles / Hyperblocks / Boxes</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Categorical & Mixed Data Classification</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Boolean and k-valued Logical Rules</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Sequential Rule Generation Algorithms</a></li>
+                
                                     </ul>
                                 </li>
-    
-                                <!-- ML ENHANCEMENT -->
+                
+                                <!-- LEVEL 2: ENHANCEMENT -->
                                 <li>
-                                    <a href="#" class="expand-btn sub-btn">
-                                        Machine Learning Enhancement methods
+                                    <a href="#">
+                                        <span class="checkbox-box"></span>
+                                        Machine Learning Enhancement Methods
                                     </a>
-    
-                                    <ul class="subtopics hidden">
-    
-                                        <li>
-                                            <a href="#">
-                                                <span class="checkbox-box"></span>
-                                                Generating synthetic data
-                                            </a>
-                                        </li>
-    
-                                        <li>
-                                            <a href="#">
-                                                <span class="checkbox-box"></span>
-                                                Model Trustworthiness
-                                            </a>
-                                        </li>
-    
+                
+                                    <ul class="dropdown">
+                
+                                        <li><a href="#"><span class="checkbox-box"></span>Estimate Worst-case Accuracy</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Labeling Data</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Generating Synthetic Data</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Tabular to Image Conversion</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Feature Learning / Representation Learning</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Dimension Reduction</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Model Boosting</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Model Trustworthiness / Explainability</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Concept Drift for n-D Time Series</a></li>
+                                        <li><a href="#"><span class="checkbox-box"></span>Class Summary View</a></li>
+                
                                     </ul>
                                 </li>
-    
+                
                             </ul>
                         </li>
-    
+                
                         <!-- VISUAL KNOWLEDGE -->
                         <li>
-                            <a href="#" class="expand-btn">
-                                Visual Knowledge Discovery Concepts
+                            <a href="#">
+                                <span class="checkbox-box"></span>
+                                Visual Knowledge Discovery (VKD) Concepts
                             </a>
-    
-                            <ul class="subtopics hidden">
-    
-                                <li>
-                                    <a href="#">
-                                        <span class="checkbox-box"></span>
-                                        Pareto Front
-                                    </a>
-                                </li>
-    
-                                <li>
-                                    <a href="#">
-                                        <span class="checkbox-box"></span>
-                                        Divide & Classify Process
-                                    </a>
-                                </li>
-    
+                
+                            <ul class="dropdown">
+                
+                                <li><a href="#"><span class="checkbox-box"></span>Full 2-D Machine Learning Paradigm</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Computational & Interactive Visual Learning</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Monotone Boolean & Ordinal Expert Knowledge Acquisition</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Divide & Classify Process</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Pareto Front</a></li>
+                
                             </ul>
                         </li>
-    
+                
+                        <!-- GENERAL LINE COORDINATES -->
+                        <li>
+                            <a href="#">
+                                <span class="checkbox-box"></span>
+                                General Line Coordinates Visualization with VKD
+                            </a>
+                
+                            <ul class="dropdown">
+                
+                                <li><a href="#"><span class="checkbox-box"></span>Parallel and Radial Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>General Line Coordinates – Linear</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Collocated Paired Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Shifted Paired Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Bended Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Glyphs</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Concentric Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Elliptic Paired Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Circular Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Dynamic Scaffolding Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>In-line Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Combined General Line Coordinates</a></li>
+                                <li><a href="#"><span class="checkbox-box"></span>Visualization of Missing Data</a></li>
+                
+                            </ul>
+                        </li>
+                
                     </ul>
                 </li>
                 <li>
