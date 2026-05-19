2ba2f236218da87a707cedd77a51b3c94af3952d
HoldRob123 <holden.robinson@comcast.net>
2026-05-19 15:20:03 -0700

    Enhance navigation bar with topic dropdowns
    Added dropdown menus for Machine Learning and Visual Knowledge topics with subcategories.

--- view/horizontal_nav_bar.php
+++ view/horizontal_nav_bar.php
@@ -11,11 +11,108 @@
                         <li><a href="#"><span class="checkbox-box"></span>Oldest</a></li>
                     </ul>
                 </li>
-                <li>
+                <li class="topic-menu">
                     <a href="#">Topic</a>
-                    <ul class="dropdown">
-                        <li><a href="#"><span class="checkbox-box"></span>AI/Machine Learning</a></li>
-                        <li><a href="#"><span class="checkbox-box"></span>Visual Knowledge</a></li>
+                
+                    <ul class="dropdown topic-dropdown">
+                
+                        <!-- MAIN CATEGORY -->
+                        <li>
+                
+                            <button class="expand-btn">
+                                ▶ Machine Learning with VKD
+                            </button>
+                
+                            <ul class="subtopics hidden">
+                
+                                <!-- SUBCATEGORY -->
+                                <li>
+                
+                                    <button class="expand-btn sub-btn">
+                                        ▶ Classification and Clustering
+                                    </button>
+                
+                                    <ul class="subtopics hidden">
+                
+                                        <li>
+                                            <a href="#">
+                                                <span class="checkbox-box"></span>
+                                                Decision Trees & Generalized Decision Trees
+                                            </a>
+                                        </li>
+                
+                                        <li>
+                                            <a href="#">
+                                                <span class="checkbox-box"></span>
+                                                Support Vector Machine
+                                            </a>
+                                        </li>
+                
+                                        <li>
+                                            <a href="#">
+                                                <span class="checkbox-box"></span>
+                                                k-Nearest Neighbors
+                                            </a>
+                                        </li>
+                
+                                    </ul>
+                                </li>
+                
+                                <!-- SECOND SUBCATEGORY -->
+                                <li>
+                
+                                    <button class="expand-btn sub-btn">
+                                        ▶ Machine Learning Enhancement methods
+                                    </button>
+                
+                                    <ul class="subtopics hidden">
+                
+                                        <li>
+                                            <a href="#">
+                                                <span class="checkbox-box"></span>
+                                                Generating synthetic data
+                                            </a>
+                                        </li>
+                
+                                        <li>
+                                            <a href="#">
+                                                <span class="checkbox-box"></span>
+                                                Model Trustworthiness
+                                            </a>
+                                        </li>
+                
+                                    </ul>
+                                </li>
+                
+                            </ul>
+                        </li>
+                
+                        <!-- SECOND MAIN CATEGORY -->
+                        <li>
+                
+                            <button class="expand-btn">
+                                ▶ Visual Knowledge Discovery Concepts
+                            </button>
+                
+                            <ul class="subtopics hidden">
+                
+                                <li>
+                                    <a href="#">
+                                        <span class="checkbox-box"></span>
+                                        Pareto Front
+                                    </a>
+                                </li>
+                
+                                <li>
+                                    <a href="#">
+                                        <span class="checkbox-box"></span>
+                                        Divide & Classify Process
+                                    </a>
+                                </li>
+                
+                            </ul>
+                        </li>
+                
                     </ul>
                 </li>
                 <li>
